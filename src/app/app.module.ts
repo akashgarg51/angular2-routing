b475c18d87f09ed56388ba4d4bee35022d408b7c
Akash Garg <akash.garg@sap.com>
2017-06-12 19:55:23 +0530

    Added user and shared folder from 84.2 zip file and changed app.module accordingly
    Change-Id: I3f3a7422e471098364e85de37ba00944715bef6f
    Signed-off-by: Akash Garg <akash.garg@sap.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,10 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { UserDetailComponent } from './user/user-detail.component'
+import { UserEditComponent } from './user/user-edit.component'
+import { UserComponent } from './user/user.component'
+
 
 @NgModule({
   declarations: [
